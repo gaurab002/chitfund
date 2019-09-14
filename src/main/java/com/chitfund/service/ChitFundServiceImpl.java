41be74515280a9d24a77c1e011ff165a81995003
gaurabsahu <gaurabsahuofc@gmail.com>
2019-09-14 11:57:57 +0530

    formula

--- src/main/java/com/chitfund/service/ChitFundServiceImpl.java
+++ src/main/java/com/chitfund/service/ChitFundServiceImpl.java
@@ -71,11 +71,11 @@ public class ChitFundServiceImpl implements IChitFundService{
 		double x =0, y=0;
 				int z = existingChitFund.getExistingChitFundCalls().size()-1;
 				double l =  existingChitFund.getExistingChitFundCalls().get(z).getCalledAmount();
-		
+		while(x > y) {
 			 x =l*terms;
 			 y = (((terms * amount) - (l*terms))/1000)* 12.50 *(terms-z) + l - 500;
-			
-		
+			 l = l-50;
+		}
 		calculateDto.setTotalProfitSoFar(totalProfit);
 		calculateDto.setProfitOnKeep(x);
 		calculateDto.setProfitOnCall(y);
