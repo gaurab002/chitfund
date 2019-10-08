22b185274e235b9c02aea6fed7fd139f968b2f5c
gaurabsahu <gaurabsahuofc@gmail.com>
2019-10-08 11:47:35 +0530

    hg

--- src/main/java/com/chitfund/pushnotification/PushNotificationService.java
+++ src/main/java/com/chitfund/pushnotification/PushNotificationService.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
+import javax.transaction.Transactional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +20,7 @@ import com.chitfund.models.DeviceToken;
 
 
 @Service
+@Transactional
 public class PushNotificationService {
 
     @Value("#{${app.notifications.defaults}}")
