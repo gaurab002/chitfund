4234211b886ff6ceab761d57ddbf61fa528b01e6
gaurabsahu <gaurabsahuofc@gmail.com>
2019-10-08 12:13:31 +0530

    schedular added

--- src/main/java/com/chitfund/ChitFundApplication.java
+++ src/main/java/com/chitfund/ChitFundApplication.java
@@ -2,8 +2,10 @@ package com.chitfund;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ChitFundApplication {
 
 	public static void main(String[] args) {
